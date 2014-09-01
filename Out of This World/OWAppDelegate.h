b93bcaefbc3d1b86b3c73bb81370ca8145eded7a
Laura <laurakirby26@gmail.com>
2014-08-31 21:59:56 -0700

    Initial Commit

---
+++ Out of This World/OWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  OWAppDelegate.h
+//  Out of This World
+//
+//  Created by Laura Kirby on 8/31/14.
+//  Copyright (c) 2014 Laura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Out of This World/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Out of This World
+//
+//  Created by Laura Kirby on 8/31/14.
+//  Copyright (c) 2014 Laura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "OWAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([OWAppDelegate class]));
+    }
+}
